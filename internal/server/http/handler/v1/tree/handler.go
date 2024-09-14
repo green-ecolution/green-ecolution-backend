f257bab359b8eba6e0ffeb9cee54270602a347cf
Cedrik Hoffmann <cedrik.hoffmann@jgdperl.com>
2024-09-14 18:36:11 +0200

    chore: merge equal functions

--- internal/server/http/handler/v1/tree/handler.go
+++ internal/server/http/handler/v1/tree/handler.go
@@ -22,6 +22,7 @@ import (
 // @Param			page			query	string	false	"Page"
 // @Param			limit			query	string	false	"Limit"
 // @Param			age				query	string	false	"Age"
+// @Param			treecluster_id	query	string	false	"Tree Cluster ID"
 // @Param			Authorization	header	string	true	"Insert your access token"	default(Bearer <Add access token here>)
 func GetAllTrees(svc service.TreeService) fiber.Handler {
 	return func(c *fiber.Ctx) error {
@@ -114,26 +115,3 @@ func DeleteTree(svc service.TreeService) fiber.Handler {
 	}
 }
 
-// @Summary		Get all trees by cluster ID
-// @Description	Get all trees by cluster ID
-// @Id				get-all-trees-by-cluster-id
-// @Tags			Trees
-// @Produce		json
-// @Success		200	{object}	tree.TreeListResponse
-// @Failure		400	{object}	HTTPError
-// @Failure		401	{object}	HTTPError
-// @Failure		403	{object}	HTTPError
-// @Failure		404	{object}	HTTPError
-// @Failure		500	{object}	HTTPError
-// @Router			/v1/tree [get]
-// @Param			page			query	string	false	"Page"
-// @Param			limit			query	string	false	"Limit"
-// @Param			age				query	string	false	"Age"
-// @Param			treecluster_id	query	string	false	"Tree Cluster ID"
-// @Param			Authorization	header	string	true	"Insert your access token"	default(Bearer <Add access token here>)
-func GetAllTreesByClusterID(svc service.TreeService) fiber.Handler {
-	return func(c *fiber.Ctx) error {
-		// TODO: Implement
-		return c.JSON(tree.TreeResponse{})
-	}
-}
