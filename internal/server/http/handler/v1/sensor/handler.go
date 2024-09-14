a72bd96121bb11c2f3340cdcbfae4d19927e9e16
Cedrik Hoffmann <cedrik.hoffmann@jgdperl.com>
2024-09-14 17:53:11 +0200

    feat: implement create and update sensor interface

--- internal/server/http/handler/v1/sensor/handler.go
+++ internal/server/http/handler/v1/sensor/handler.go
@@ -6,20 +6,20 @@ import (
 	"github.com/green-ecolution/green-ecolution-backend/internal/service"
 )
 
-// @Summary		Get all sensors
-// @Description	Get all sensors
-// @Id				get-all-sensors
-// @Tags			Sensor
-// @Produce		json
-// @Success		200	{object}	[]sensor.SensorResponse
-// @Failure		400	{object}	HTTPError
-// @Failure		401	{object}	HTTPError
-// @Failure		403	{object}	HTTPError
-// @Failure		404	{object}	HTTPError
-// @Failure		500	{object}	HTTPError
-// @Router			/v1/sensor [get]
-// @Param			status			query	string	false	"Sensor Status"
-// @Param			Authorization	header	string	true	"Insert your access token"	default(Bearer <Add access token here>)
+//	@Summary		Get all sensors
+//	@Description	Get all sensors
+//	@Id				get-all-sensors
+//	@Tags			Sensor
+//	@Produce		json
+//	@Success		200	{object}	[]sensor.SensorResponse
+//	@Failure		400	{object}	HTTPError
+//	@Failure		401	{object}	HTTPError
+//	@Failure		403	{object}	HTTPError
+//	@Failure		404	{object}	HTTPError
+//	@Failure		500	{object}	HTTPError
+//	@Router			/v1/sensor [get]
+//	@Param			status			query	string	false	"Sensor Status"
+//	@Param			Authorization	header	string	true	"Insert your access token"	default(Bearer <Add access token here>)
 func GetAllSensor(_ service.Service) fiber.Handler {
 	return func(c *fiber.Ctx) error {
 		// TODO: Implement GetAll
@@ -27,20 +27,20 @@ func GetAllSensor(_ service.Service) fiber.Handler {
 	}
 }
 
-// @Summary		Get sensor by ID
-// @Description	Get sensor by ID
-// @Id				get-sensor-by-id
-// @Tags			Sensor
-// @Produce		json
-// @Success		200	{object}	sensor.SensorResponse
-// @Failure		400	{object}	HTTPError
-// @Failure		401	{object}	HTTPError
-// @Failure		403	{object}	HTTPError
-// @Failure		404	{object}	HTTPError
-// @Failure		500	{object}	HTTPError
-// @Router			/v1/sensor/{sensor_id} [get]
-// @Param			sensor_id		path	string	true	"Sensor ID"
-// @Param			Authorization	header	string	true	"Insert your access token"	default(Bearer <Add access token here>)
+//	@Summary		Get sensor by ID
+//	@Description	Get sensor by ID
+//	@Id				get-sensor-by-id
+//	@Tags			Sensor
+//	@Produce		json
+//	@Success		200	{object}	sensor.SensorResponse
+//	@Failure		400	{object}	HTTPError
+//	@Failure		401	{object}	HTTPError
+//	@Failure		403	{object}	HTTPError
+//	@Failure		404	{object}	HTTPError
+//	@Failure		500	{object}	HTTPError
+//	@Router			/v1/sensor/{sensor_id} [get]
+//	@Param			sensor_id		path	string	true	"Sensor ID"
+//	@Param			Authorization	header	string	true	"Insert your access token"	default(Bearer <Add access token here>)
 func GetSensorByID(_ service.Service) fiber.Handler {
 	return func(c *fiber.Ctx) error {
 		// TODO: Implement GetByID
@@ -48,25 +48,68 @@ func GetSensorByID(_ service.Service) fiber.Handler {
 	}
 }
 
-// @Summary		Get sensor data by ID
-// @Description	Get sensor data by ID
-// @Id				get-sensor-data-by-id
-// @Tags			Sensor
-// @Produce		json
-// @Success		200	{object}	sensor.SensorDataResponse
-// @Failure		400	{object}	HTTPError
-// @Failure		401	{object}	HTTPError
-// @Failure		403	{object}	HTTPError
-// @Failure		404	{object}	HTTPError
-// @Failure		500	{object}	HTTPError
-// @Router			/v1/sensor/{sensor_id}/data [get]
-// @Param			sensor_id		path	string	true	"Sensor ID"
-// @Param			page			query	string	false	"Page"
-// @Param			limit			query	string	false	"Limit"
-// @Param			start_time		query	string	false	"Start time"
-// @Param			end_time		query	string	false	"End time"
-// @Param			Authorization	header	string	false	"Insert your access token"	default(Bearer <Add access token here>)
+//	@Summary		Get sensor data by ID
+//	@Description	Get sensor data by ID
+//	@Id				get-sensor-data-by-id
+//	@Tags			Sensor
+//	@Produce		json
+//	@Success		200	{object}	sensor.SensorDataResponse
+//	@Failure		400	{object}	HTTPError
+//	@Failure		401	{object}	HTTPError
+//	@Failure		403	{object}	HTTPError
+//	@Failure		404	{object}	HTTPError
+//	@Failure		500	{object}	HTTPError
+//	@Router			/v1/sensor/{sensor_id}/data [get]
+//	@Param			sensor_id		path	string	true	"Sensor ID"
+//	@Param			page			query	string	false	"Page"
+//	@Param			limit			query	string	false	"Limit"
+//	@Param			start_time		query	string	false	"Start time"
+//	@Param			end_time		query	string	false	"End time"
+//	@Param			Authorization	header	string	false	"Insert your access token"	default(Bearer <Add access token here>)
 func GetSensorDataByID(_ service.Service) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		// TODO: Implement GetByID
+		return c.JSON(sensor.SensorDataResponse{})
+	}
+}
+
+//	@Summary		Create sensor
+//	@Description	Create sensor
+//	@Id				create-sensor
+//	@Tags			Sensor
+//	@Produce		json
+//	@Success		200	{object}	sensor.SensorDataResponse
+//	@Failure		400	{object}	HTTPError
+//	@Failure		401	{object}	HTTPError
+//	@Failure		403	{object}	HTTPError
+//	@Failure		404	{object}	HTTPError
+//	@Failure		500	{object}	HTTPError
+//	@Router			/v1/sensor/ [post]
+//	@Param			Authorization	header	string						false	"Insert your access token"	default(Bearer <Add access token here>)
+//	@Param			body			body	sensor.SensorCreateRequest	true	"Sensor to create"
+func CreateSensor(_ service.Service) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		// TODO: Implement GetByID
+		return c.JSON(sensor.SensorDataResponse{})
+	}
+}
+
+//	@Summary		Update sensor
+//	@Description	Update sensor
+//	@Id				update-sensor
+//	@Tags			Sensor
+//	@Produce		json
+//	@Success		200	{object}	sensor.SensorDataResponse
+//	@Failure		400	{object}	HTTPError
+//	@Failure		401	{object}	HTTPError
+//	@Failure		403	{object}	HTTPError
+//	@Failure		404	{object}	HTTPError
+//	@Failure		500	{object}	HTTPError
+//	@Router			/v1/sensor/{sensor_id} [patch]
+//	@Param			sensor_id		path	string						true	"Sensor ID"
+//	@Param			Authorization	header	string						false	"Insert your access token"	default(Bearer <Add access token here>)
+//	@Param			body			body	sensor.SensorUpdateRequest	true	"Sensor information to update"
+func UpdateSensor(_ service.Service) fiber.Handler {
 	return func(c *fiber.Ctx) error {
 		// TODO: Implement GetByID
 		return c.JSON(sensor.SensorDataResponse{})
