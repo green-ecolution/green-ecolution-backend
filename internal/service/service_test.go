753615d3c9e022b3549e532f5988a5e75eed5a3b
Cedrik Hoffmann <cedrik.hoffmann@jgdperl.com>
2024-09-26 01:56:00 +0200

    chore: go fmt

--- internal/service/service_test.go
+++ internal/service/service_test.go
@@ -14,13 +14,13 @@ func TestAllServiceReady(t *testing.T) {
 		mqttSvc := serviceMock.NewMockMqttService(t)
 		treeSvc := serviceMock.NewMockTreeService(t)
 		authSvc := serviceMock.NewMockAuthService(t)
-    regionSvc := serviceMock.NewMockRegionService(t)
+		regionSvc := serviceMock.NewMockRegionService(t)
 		svc := Services{
-			InfoService: infoSvc,
-			MqttService: mqttSvc,
-			TreeService: treeSvc,
-			AuthService: authSvc,
-      RegionService: regionSvc,
+			InfoService:   infoSvc,
+			MqttService:   mqttSvc,
+			TreeService:   treeSvc,
+			AuthService:   authSvc,
+			RegionService: regionSvc,
 		}
 
 		// when
@@ -28,7 +28,7 @@ func TestAllServiceReady(t *testing.T) {
 		mqttSvc.EXPECT().Ready().Return(true)
 		treeSvc.EXPECT().Ready().Return(true)
 		authSvc.EXPECT().Ready().Return(true)
-    regionSvc.EXPECT().Ready().Return(true)
+		regionSvc.EXPECT().Ready().Return(true)
 
 		ready := svc.AllServicesReady()
 
