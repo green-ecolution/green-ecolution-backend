66122ded06f36e25ee5789353535c74937c1037e
Cedrik Hoffmann <choffmann@progeek.de>
2024-10-02 15:14:37 +0200

    feat: register plugin and implement cleanup worker

--- internal/server/http/handler/v1/plugin/handler.go
+++ internal/server/http/handler/v1/plugin/handler.go
@@ -1,16 +1,19 @@
 package plugin
 
 import (
+	"log/slog"
 	"net/http/httputil"
 	"net/url"
 	"strings"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/adaptor"
+	"github.com/spf13/viper"
 )
 
 func getPluginFiles(c *fiber.Ctx) error {
-	url, err := url.Parse("http://localhost:3000")
+	url, err := url.Parse(viper.GetString("server.app_url"))
 	if err != nil {
 		return err
 	}
@@ -27,4 +30,51 @@ func getPluginFiles(c *fiber.Ctx) error {
 	}
 
 	return adaptor.HTTPHandler(&reverseProxy)(c)
+}
+
+type PluginRegisterRequest struct {
+	Name string `json:"name"`
+	Host string `json:"host"`
+	Path string `json:"path"`
+}
+
+func registerPlugin() fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		var req PluginRegisterRequest
+		if err := c.BodyParser(&req); err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": err.Error(),
+			})
+		}
+
+		pluginMutex.Lock()
+		defer pluginMutex.Unlock()
+
+		if _, ok := registeredPlugins[req.Name]; ok {
+			return c.Status(fiber.StatusForbidden).SendString("plugin already registered")
+		}
+
+		registeredPlugins[req.Name] = Plugin{
+			Name:          req.Name,
+			Path:          req.Path,
+			Host:          req.Host,
+			LastHeartbeat: time.Now(),
+		}
+
+    slog.Info("Plugin registered", "plugin", req.Name)
+		return c.Status(fiber.StatusOK).SendString("Plugin registered")
+	}
+}
+
+func pluginHeartbeat() fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		pluginMutex.Lock()
+		defer pluginMutex.Unlock()
+
+		registeredPlugins[c.Params("plugin")] = Plugin{
+			LastHeartbeat: time.Now(),
+		}
+
+		return c.Status(fiber.StatusOK).SendString("Heartbeat received")
+	}
 }
---
+++ internal/server/http/handler/v1/plugin/worker.go
@@ -0,0 +1,66 @@
+package plugin
+
+import (
+	"context"
+	"log/slog"
+	"time"
+)
+
+type PluginWorkerConfig struct {
+	interval time.Duration
+	timeout  time.Duration
+}
+
+type PluginWorker struct {
+	cfg PluginWorkerConfig
+}
+
+type PluginWorkerOption func(*PluginWorkerConfig)
+
+var defaultPluginWorkerConfig = PluginWorkerConfig{
+	timeout:  5 * time.Second,
+	interval: 1 * time.Second,
+}
+
+func WithTimeout(timeout time.Duration) PluginWorkerOption {
+	return func(cfg *PluginWorkerConfig) {
+		cfg.timeout = timeout
+	}
+}
+
+func WithInterval(interval time.Duration) PluginWorkerOption {
+	return func(cfg *PluginWorkerConfig) {
+		cfg.interval = interval
+	}
+}
+
+func NewPluginWorker(opts ...PluginWorkerOption) *PluginWorker {
+	cfg := defaultPluginWorkerConfig
+	for _, opt := range opts {
+		opt(&cfg)
+	}
+
+	return &PluginWorker{
+		cfg: cfg,
+	}
+}
+
+func (w *PluginWorker) Run(ctx context.Context) error {
+	ticker := time.NewTicker(w.cfg.interval)
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case <-ticker.C:
+			pluginMutex.Lock()
+			for name, plugin := range registeredPlugins {
+				if time.Since(plugin.LastHeartbeat) > w.cfg.timeout {
+          slog.Info("Removing plugin due to timeout", "plugin", name)
+					delete(registeredPlugins, name)
+				}
+			}
+			pluginMutex.Unlock()
+		}
+	}
+}
+
