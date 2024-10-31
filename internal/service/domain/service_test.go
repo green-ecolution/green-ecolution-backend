9083628352b7ce281473480a0b932ee6ec35380c
Dorien Grönwald <dorien.groenwald@visuellverstehen.de>
2024-10-31 12:39:27 +0100

    fix: add test case

--- internal/service/domain/service_test.go
+++ internal/service/domain/service_test.go
@@ -11,36 +11,38 @@ import (
 )
 
 func TestNewService(t *testing.T) {
-	mockConfig := &config.Config{}
-	mockClusterRepo := storageMock.NewMockTreeClusterRepository(t)
-	mockTreeRepo := storageMock.NewMockTreeRepository(t)
-	mockRegionRepo := storageMock.NewMockRegionRepository(t)
-	mockInfoRepo := storageMock.NewMockInfoRepository(t)
-	mockSensorRepo := storageMock.NewMockSensorRepository(t)
-	mockAuthRepo := storageMock.NewMockAuthRepository(t)
-	mockUserRepo := storageMock.NewMockUserRepository(t)
-	mockImageRepo := storageMock.NewMockImageRepository(t)
+	t.Run("should initialize service with all repoistories", func(t *testing.T) {
+		mockConfig := &config.Config{}
+		mockClusterRepo := storageMock.NewMockTreeClusterRepository(t)
+		mockTreeRepo := storageMock.NewMockTreeRepository(t)
+		mockRegionRepo := storageMock.NewMockRegionRepository(t)
+		mockInfoRepo := storageMock.NewMockInfoRepository(t)
+		mockSensorRepo := storageMock.NewMockSensorRepository(t)
+		mockAuthRepo := storageMock.NewMockAuthRepository(t)
+		mockUserRepo := storageMock.NewMockUserRepository(t)
+		mockImageRepo := storageMock.NewMockImageRepository(t)
 
-	mockRepos := &storage.Repository{
-		Auth:        mockAuthRepo,
-		Info:        mockInfoRepo,
-		Sensor:      mockSensorRepo,
-		Tree:        mockTreeRepo,
-		User:        mockUserRepo,
-		Image:       mockImageRepo,
-		TreeCluster: mockClusterRepo,
-		Region:      mockRegionRepo,
-	}
+		mockRepos := &storage.Repository{
+			Auth:        mockAuthRepo,
+			Info:        mockInfoRepo,
+			Sensor:      mockSensorRepo,
+			Tree:        mockTreeRepo,
+			User:        mockUserRepo,
+			Image:       mockImageRepo,
+			TreeCluster: mockClusterRepo,
+			Region:      mockRegionRepo,
+		}
 
-	svc := NewService(mockConfig, mockRepos)
+		svc := NewService(mockConfig, mockRepos)
 
-	assert.NotNil(t, svc)
-	assert.IsType(t, &service.Services{}, svc)
-	assert.NotNil(t, svc.InfoService)
-	assert.NotNil(t, svc.MqttService)
-	assert.NotNil(t, svc.TreeService)
-	assert.NotNil(t, svc.AuthService)
-	assert.NotNil(t, svc.RegionService)
-	assert.NotNil(t, svc.TreeClusterService)
-	assert.NotNil(t, svc.SensorService)
+		assert.NotNil(t, svc)
+		assert.IsType(t, &service.Services{}, svc)
+		assert.NotNil(t, svc.InfoService)
+		assert.NotNil(t, svc.MqttService)
+		assert.NotNil(t, svc.TreeService)
+		assert.NotNil(t, svc.AuthService)
+		assert.NotNil(t, svc.RegionService)
+		assert.NotNil(t, svc.TreeClusterService)
+		assert.NotNil(t, svc.SensorService)
+	})
 }
