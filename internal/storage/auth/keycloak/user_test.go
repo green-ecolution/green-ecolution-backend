9133a9b40e4e322e3b73e063bfa0247b53199d40
Cedrik Hoffmann <cedrik.hoffmann@jgdperl.com>
2024-11-02 21:45:50 +0100

    chore: turn into table driven test design

--- internal/storage/auth/keycloak/user_test.go
+++ internal/storage/auth/keycloak/user_test.go
@@ -2,195 +2,182 @@ package keycloak
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
+	"github.com/green-ecolution/green-ecolution-backend/config"
 	"github.com/green-ecolution/green-ecolution-backend/internal/entities"
 	"github.com/stretchr/testify/assert"
 )
 
+var testUser = testUserToCreateFunc()
+
 func TestKeyCloakUserRepo_Create(t *testing.T) {
-	t.Run("should create user successfully", func(t *testing.T) {
-		// given
-		ctx := context.Background()
-		cfg := suite.IdentityConfig(t, ctx)
-		repo := NewUserRepository(cfg)
+	type fields struct {
+		cfg *config.IdentityAuthConfig
+	}
+	type args struct {
+		ctx      context.Context
+		user     *entities.User
+		password string
+		roles    []string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    *entities.User
+		wantErr bool
+	}{
+		{
+			name: "should create user successfully",
+			fields: fields{
+				cfg: suite.IdentityConfig(t, context.Background()),
+			},
+			args: args{
+				ctx:      context.Background(),
+				user:     testUser[0],
+				password: "test",
+				roles:    []string{},
+			},
+			wantErr: false,
+		},
+		{
+			name: "should return error when create with same email",
+			fields: fields{
+				cfg: suite.IdentityConfig(t, context.Background()),
+			},
+			args: args{
+				ctx:      context.Background(),
+				user:     testUser[0],
+				password: "test",
+				roles:    []string{},
+			},
+			wantErr: true,
+		},
+		{
+			name: "should return error when create with same username",
+			fields: fields{
+				cfg: suite.IdentityConfig(t, context.Background()),
+			},
+			args: args{
+				ctx: context.Background(),
+				user: &entities.User{
+					Username:    testUser[0].Username,
+					FirstName:   testUser[1].FirstName,
+					LastName:    testUser[1].LastName,
+					Email:       testUser[1].Email,
+					EmployeeID:  testUser[1].EmployeeID,
+					PhoneNumber: testUser[1].PhoneNumber,
+				},
+				password: "test",
+				roles:    []string{},
+			},
+			wantErr: true,
+		},
+		{
+			name: "should create user successfully with role",
+			fields: fields{
+				cfg: suite.IdentityConfig(t, context.Background()),
+			},
+			args: args{
+				ctx:      context.Background(),
+				user:     testUser[2],
+				password: "test",
+				roles:    []string{"admin"},
+			},
+			wantErr: false,
+		},
+		{
+			name: "should create user successfully when roles are nil",
+			fields: fields{
+				cfg: suite.IdentityConfig(t, context.Background()),
+			},
+			args: args{
+				ctx:      context.Background(),
+				user:     testUser[3],
+				password: "test",
+				roles:    nil,
+			},
+			wantErr: false,
+		},
+		{
+			name: "should return error when user is nil",
+			fields: fields{
+				cfg: suite.IdentityConfig(t, context.Background()),
+			},
+			args: args{
+				ctx:      context.Background(),
+				user:     nil,
+				password: "test",
+				roles:    nil,
+			},
+			wantErr: true,
+		},
+		{
+			name: "should return error when role not exist",
+			fields: fields{
+				cfg: suite.IdentityConfig(t, context.Background()),
+			},
+			args: args{
+				ctx:      context.Background(),
+				user:     testUser[4],
+				password: "test",
+				roles:    []string{"not-exist"},
+			},
+			wantErr: true,
+		},
+		{
+			name: "should return error when failed to set password",
+			fields: fields{
+				cfg: suite.IdentityConfig(t, context.Background()),
+			},
+			args: args{
+				ctx:      context.Background(),
+				user:     testUser[5],
+				password: "",
+				roles:    nil,
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := &UserRepository{
+				cfg: tt.fields.cfg,
+			}
+			got, err := r.Create(tt.args.ctx, tt.args.user, tt.args.password, tt.args.roles)
+			if tt.wantErr {
+				assert.Error(t, err)
+				assert.Nil(t, got)
+			} else {
+				assert.NotNil(t, got)
+				assert.Equal(t, tt.args.user.Username, got.Username)
+				assert.Equal(t, tt.args.user.FirstName, got.FirstName)
+				assert.Equal(t, tt.args.user.LastName, got.LastName)
+				assert.Equal(t, tt.args.user.Email, got.Email)
+				assert.Equal(t, tt.args.user.EmployeeID, got.EmployeeID)
+				assert.Equal(t, tt.args.user.PhoneNumber, got.PhoneNumber)
+				assert.NotNil(t, got.ID)
+				assert.NotZero(t, got.ID)
+				assert.NotZero(t, got.CreatedAt)
+			}
+		})
+	}
+}
 
-		password := "test"
-		user := &entities.User{
-			Username:    "test",
+func testUserToCreateFunc() []*entities.User {
+	n := 20
+	users := make([]*entities.User, n)
+	for i := 0; i < n; i++ {
+		users[i] = &entities.User{
+			Username:    fmt.Sprintf("test%d", i),
 			FirstName:   "Toni",
 			LastName:    "Tester",
-			Email:       "test@green-ecolution.de",
+			Email:       fmt.Sprintf("test%d@green-ecolution.de", i),
 			EmployeeID:  "123456",
 			PhoneNumber: "+49 123456",
 		}
-
-		// when
-		createdUser, err := repo.Create(ctx, user, password, []string{})
-
-		// then
-		assert.NoError(t, err)
-		assert.NotNil(t, createdUser)
-		assert.Equal(t, user.Username, createdUser.Username)
-		assert.Equal(t, user.FirstName, createdUser.FirstName)
-		assert.Equal(t, user.LastName, createdUser.LastName)
-		assert.Equal(t, user.Email, createdUser.Email)
-		assert.Equal(t, user.EmployeeID, createdUser.EmployeeID)
-		assert.Equal(t, user.PhoneNumber, createdUser.PhoneNumber)
-		assert.NotNil(t, createdUser.ID)
-		assert.NotZero(t, createdUser.ID)
-		assert.NotZero(t, createdUser.CreatedAt)
-	})
-
-	t.Run("should return error when create with same email", func(t *testing.T) {
-		// given
-		ctx := context.Background()
-		cfg := suite.IdentityConfig(t, ctx)
-		repo := NewUserRepository(cfg)
-
-		password := "test"
-		user := &entities.User{
-			Username:    "test",
-			FirstName:   "Toni",
-			LastName:    "Tester",
-			Email:       "test@green-ecolution.de",
-			EmployeeID:  "123456",
-			PhoneNumber: "+49 123456",
-		}
-
-		// when
-		createdUser, err := repo.Create(ctx, user, password, []string{})
-
-		// then
-		assert.Error(t, err)
-		assert.Nil(t, createdUser)
-	})
-
-	t.Run("should return error when create with same username", func(t *testing.T) {
-		// given
-		ctx := context.Background()
-		cfg := suite.IdentityConfig(t, ctx)
-		repo := NewUserRepository(cfg)
-
-		password := "test"
-		user := &entities.User{
-			Username:    "test",
-			FirstName:   "Toni",
-			LastName:    "Tester",
-			Email:       "test1@green-ecolution.de",
-			EmployeeID:  "123456",
-			PhoneNumber: "+49 123456",
-		}
-
-		// when
-		createdUser, err := repo.Create(ctx, user, password, []string{})
-
-		// then
-		assert.Error(t, err)
-		assert.Nil(t, createdUser)
-	})
-
-	t.Run("should create user successfully with role", func(t *testing.T) {
-		// given
-		ctx := context.Background()
-		cfg := suite.IdentityConfig(t, ctx)
-		repo := NewUserRepository(cfg)
-
-		password := "test"
-		user := &entities.User{
-			Username:    "test2",
-			FirstName:   "Toni",
-			LastName:    "Tester",
-			Email:       "test2@green-ecolution.de",
-			EmployeeID:  "123456",
-			PhoneNumber: "+49 123456",
-		}
-
-		// when
-		createdUser, err := repo.Create(ctx, user, password, []string{"admin"})
-
-		// then
-		assert.NoError(t, err)
-		assert.NotNil(t, createdUser)
-		assert.Equal(t, user.Username, createdUser.Username)
-		assert.Equal(t, user.FirstName, createdUser.FirstName)
-		assert.Equal(t, user.LastName, createdUser.LastName)
-		assert.Equal(t, user.Email, createdUser.Email)
-		assert.Equal(t, user.EmployeeID, createdUser.EmployeeID)
-		assert.Equal(t, user.PhoneNumber, createdUser.PhoneNumber)
-		assert.NotNil(t, createdUser.ID)
-		assert.NotZero(t, createdUser.ID)
-		assert.NotZero(t, createdUser.CreatedAt)
-	})
-
-	t.Run("should create user successfully when roles are nil", func(t *testing.T) {
-		// given
-		ctx := context.Background()
-		cfg := suite.IdentityConfig(t, ctx)
-		repo := NewUserRepository(cfg)
-
-		password := "test"
-		user := &entities.User{
-			Username:    "test3",
-			FirstName:   "Toni",
-			LastName:    "Tester",
-			Email:       "test3@green-ecolution.de",
-			EmployeeID:  "123456",
-			PhoneNumber: "+49 123456",
-		}
-
-		// when
-		createdUser, err := repo.Create(ctx, user, password, nil)
-
-		// then
-		assert.NoError(t, err)
-		assert.NotNil(t, createdUser)
-		assert.Equal(t, user.Username, createdUser.Username)
-		assert.Equal(t, user.FirstName, createdUser.FirstName)
-		assert.Equal(t, user.LastName, createdUser.LastName)
-		assert.Equal(t, user.Email, createdUser.Email)
-		assert.Equal(t, user.EmployeeID, createdUser.EmployeeID)
-		assert.Equal(t, user.PhoneNumber, createdUser.PhoneNumber)
-		assert.NotNil(t, createdUser.ID)
-		assert.NotZero(t, createdUser.ID)
-		assert.NotZero(t, createdUser.CreatedAt)
-	})
-
-	t.Run("should return error when user is nil", func(t *testing.T) {
-		// given
-		ctx := context.Background()
-		cfg := suite.IdentityConfig(t, ctx)
-		repo := NewUserRepository(cfg)
-
-		// when
-		createdUser, err := repo.Create(ctx, nil, "test", nil)
-
-		// then
-		assert.Error(t, err)
-		assert.Nil(t, createdUser)
-	})
-
-	t.Run("should return error when role not exist", func(t *testing.T) {
-		// given
-		ctx := context.Background()
-		cfg := suite.IdentityConfig(t, ctx)
-		repo := NewUserRepository(cfg)
-
-		password := "test"
-		user := &entities.User{
-			Username:    "test4",
-			FirstName:   "Toni",
-			LastName:    "Tester",
-			Email:       "test4@green-ecolution.de",
-			EmployeeID:  "123456",
-			PhoneNumber: "123456",
-		}
-
-		// when
-		createdUser, err := repo.Create(ctx, user, password, []string{"not-exist"})
-
-		// then
-		assert.Error(t, err)
-		assert.Nil(t, createdUser)
-	})
+	}
+	return users
 }
