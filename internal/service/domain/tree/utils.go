268dbb1b5b5e3a35b61577c8e685120ad290ab60
Mohammad Assaf <mohammadassafb@gmail.com>
2024-11-03 00:49:39 +0100

    chor: imporve the usage of the utils file

--- internal/service/domain/tree/utils.go
+++ internal/service/domain/tree/utils.go
@@ -3,6 +3,8 @@ package tree
 import (
 	"time"
 
+	"github.com/green-ecolution/green-ecolution-backend/internal/utils"
+
 	"github.com/green-ecolution/green-ecolution-backend/internal/entities"
 )
 
@@ -19,8 +21,8 @@ var (
 			Description:   "Test description",
 			SoilCondition: entities.TreeSoilConditionLehmig,
 			Archived:      false,
-			Latitude:      float64Ptr(testLatitude),
-			Longitude:     float64Ptr(testLongitude),
+			Latitude:      utils.P(testLatitude),
+			Longitude:     utils.P(testLongitude),
 			Trees:         TestTreesList,
 		},
 		{
@@ -45,8 +47,8 @@ var (
 			UpdatedAt:    time.Now(),
 			Species:      "Oak",
 			Number:       "T001",
-			Latitude:     9.446741,
-			Longitude:    54.801539,
+			Latitude:     testLatitude,
+			Longitude:    testLongitude,
 			Description:  "A mature oak tree",
 			PlantingYear: 2023,
 			Readonly:     true,
@@ -81,12 +83,33 @@ var (
 			Data:      nil,
 		},
 	}
+
+	TestTreeCreate = &entities.TreeCreate{
+		Species:       "Oak",
+		Latitude:      testLatitude,
+		Longitude:     testLongitude,
+		PlantingYear:  2023,
+		Number:        "T001",
+		TreeClusterID: utils.PtrInt32(1),
+		SensorID:      utils.PtrInt32(1),
+	}
+
+	TestTreeImport = &entities.TreeImport{
+		Latitude:     testLatitude,
+		Longitude:    testLongitude,
+		PlantingYear: 2023,
+		Species:      "Oak",
+		Number:       "T001",
+	}
+
+	TestTreeUpdate = &entities.TreeUpdate{
+		TreeClusterID: utils.PtrInt32(1),
+		SensorID:      utils.PtrInt32(1),
+		PlantingYear:  2023,
+		Species:       "Oak",
+		Number:        "T001",
+		Latitude:      testLatitude,
+		Longitude:     testLongitude,
+		Description:   "Updated description",
+	}
 )
-
-func float64Ptr(f float64) *float64 {
-	return &f
-}
-
-func ptrToInt32(i int32) *int32 {
-	return &i
-}
