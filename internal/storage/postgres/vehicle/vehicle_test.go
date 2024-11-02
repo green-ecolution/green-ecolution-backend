1f9e4306a78de792ef7a8855e2990a30af281dc4
Dorien Grönwald <dorien.groenwald@visuellverstehen.de>
2024-11-02 19:01:13 +0100

    feat: add delete and get tests for vehicles

---
+++ internal/storage/postgres/vehicle/vehicle_test.go
@@ -0,0 +1,80 @@
+package vehicle
+
+import (
+	"context"
+	"os"
+	"testing"
+
+	"github.com/green-ecolution/green-ecolution-backend/internal/storage/postgres/mapper/generated"
+	store "github.com/green-ecolution/green-ecolution-backend/internal/storage/postgres/store"
+	"github.com/green-ecolution/green-ecolution-backend/internal/storage/postgres/testutils"
+	"github.com/stretchr/testify/assert"
+)
+
+type vehicleFields struct {
+	store         *store.Store
+	VehicleMappers VehicleRepositoryMappers
+}
+
+var (
+	defaultFields vehicleFields
+	suite         *testutils.PostgresTestSuite
+)
+
+func defaultVehicleMappers() VehicleRepositoryMappers {
+	return NewVehicleRepositoryMappers(&generated.InternalVehicleRepoMapperImpl{})
+}
+
+func TestMain(m *testing.M) {
+	code := 1
+	ctx := context.Background()
+	defer func() { os.Exit(code) }()
+	suite = testutils.SetupPostgresTestSuite(ctx)
+	defaultFields = vehicleFields{
+		store:         suite.Store,
+		VehicleMappers: defaultVehicleMappers(),
+	}
+	defer suite.Terminate(ctx)
+
+	code = m.Run()
+}
+
+func TestVehicleRepository_Delete(t *testing.T) {
+	t.Run("should delete vehicle", func(t *testing.T) {
+		// given
+		suite.ResetDB(t)
+		suite.InsertSeed(t, "internal/storage/postgres/seed/test/vehicle")
+		r := NewVehicleRepository(defaultFields.store, defaultFields.VehicleMappers)
+
+		// when
+		err := r.Delete(context.Background(), 1)
+
+		// then
+		assert.NoError(t, err)
+	})
+
+	t.Run("should return error when vehicle not found", func(t *testing.T) {
+		// given
+		suite.ResetDB(t)
+		r := NewVehicleRepository(defaultFields.store, defaultFields.VehicleMappers)
+
+		// when
+		err := r.Delete(context.Background(), 99)
+
+		// then
+		assert.Error(t, err)
+	})
+
+	t.Run("should return error when vehicle is canceled", func(t *testing.T) {
+		// given
+		r := NewVehicleRepository(defaultFields.store, defaultFields.VehicleMappers)
+		ctx, cancel := context.WithCancel(context.Background())
+		cancel()
+
+		// when
+		err := r.Delete(ctx, 1)
+
+		// then
+		assert.Error(t, err)
+	})
+}
