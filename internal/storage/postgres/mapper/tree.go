f7602286d1196b97b07dc1f4ad66c3abb3085a82
Dorien Grönwald <dorien.groenwald@visuellverstehen.de>
2024-11-02 18:21:43 +0100

    fix: add mapping functions for goverter

--- internal/storage/postgres/mapper/tree.go
+++ internal/storage/postgres/mapper/tree.go
@@ -8,6 +8,8 @@ import (
 // goverter:converter
 // goverter:extend github.com/green-ecolution/green-ecolution-backend/internal/utils:PgTimestampToTime
 // goverter:extend github.com/green-ecolution/green-ecolution-backend/internal/utils:PgTimestampToTimePtr
+// goverter:extend github.com/green-ecolution/green-ecolution-backend/internal/utils:StringPtrToString
+// goverter:extend MapWateringStatus MapSoilCondition
 type InternalTreeRepoMapper interface {
 	// goverter:ignore Sensor Images TreeCluster
 	// goverter:map TreeNumber Number
