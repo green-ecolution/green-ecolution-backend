5aa0c0c6d9d93586bc15d78f3bc304ed7bb90257
Dorien Grönwald <dorien.groenwald@visuellverstehen.de>
2024-11-03 13:12:57 +0100

    feat: add create vehicle test

--- internal/storage/postgres/vehicle/create.go
+++ internal/storage/postgres/vehicle/create.go
@@ -2,6 +2,7 @@ package vehicle
 
 import (
 	"context"
+	"errors"
 
 	"github.com/green-ecolution/green-ecolution-backend/internal/entities"
 	sqlc "github.com/green-ecolution/green-ecolution-backend/internal/storage/postgres/_sqlc"
@@ -11,7 +12,7 @@ func defaultVehicle() *entities.Vehicle {
 	return &entities.Vehicle{
 		NumberPlate:   "",
 		Description:   "",
-		WaterCapacity: 100,
+		WaterCapacity: 0,
 	}
 }
 
@@ -19,6 +20,14 @@ func (r *VehicleRepository) Create(ctx context.Context, vFn ...entities.EntityFu
 	entity := defaultVehicle()
 	for _, fn := range vFn {
 		fn(entity)
+	}
+
+	if entity.WaterCapacity == 0 {
+		return nil, errors.New("water capacity is required and can not be 0")
+	}
+
+	if entity.NumberPlate == "" {
+		return nil, errors.New("number plate is required")
 	}
 
 	id, err := r.createEntity(ctx, entity)
