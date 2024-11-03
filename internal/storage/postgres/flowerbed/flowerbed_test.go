cf2adb2be1c88a18204f8c7c2d84ef74ce6610ef
Dorien Grönwald <dorien.groenwald@visuellverstehen.de>
2024-11-03 15:50:57 +0100

    feat: add delete tests for flowerbeds

--- internal/storage/postgres/flowerbed/flowerbed_test.go
+++ internal/storage/postgres/flowerbed/flowerbed_test.go
@@ -239,3 +239,80 @@ func TestRegionRepository_UnlinkAllImages(t *testing.T) {
 		assert.Error(t, err)
 	})
 }
+
+func TestTreeClusterRepository_Archived(t *testing.T) {
+	t.Run("should archive flowerbed", func(t *testing.T) {
+		// given
+		suite.ResetDB(t)
+		suite.InsertSeed(t, "internal/storage/postgres/seed/test/flowerbed")
+		r := NewFlowerbedRepository(suite.Store, mappers)
+
+		// when
+		err := r.Archive(context.Background(), 1)
+		got, errGot := r.GetByID(context.Background(), 1)
+
+		// then
+		assert.NoError(t, err)
+		assert.NoError(t, errGot)
+		assert.NotNil(t, got)
+		assert.True(t, got.Archived)
+	})
+
+	t.Run("should return error when flowerbed with non-existing id", func(t *testing.T) {
+		// given
+		r := NewFlowerbedRepository(suite.Store, mappers)
+
+		// when
+		err := r.Archive(context.Background(), 99)
+
+		// then
+		assert.Error(t, err)
+	})
+
+	t.Run("should return error when flowerbed with negative id", func(t *testing.T) {
+		// given
+		r := NewFlowerbedRepository(suite.Store, mappers)
+
+		// when
+		err := r.Archive(context.Background(), -1)
+
+		// then
+		assert.Error(t, err)
+	})
+
+	t.Run("should not return error when archive flowerbed twice", func(t *testing.T) {
+		// given
+		suite.ResetDB(t)
+		suite.InsertSeed(t, "internal/storage/postgres/seed/test/flowerbed")
+		r := NewFlowerbedRepository(suite.Store, mappers)
+		err := r.Archive(context.Background(), 1)
+		assert.NoError(t, err)
+
+		// when
+		gotBefore, errGotBefore := r.GetByID(context.Background(), 1)
+		err = r.Archive(context.Background(), 1)
+		gotAfter, errGotAfter := r.GetByID(context.Background(), 1)
+
+		// then
+		assert.NoError(t, err)
+		assert.NoError(t, errGotBefore)
+		assert.NoError(t, errGotAfter)
+		assert.NotNil(t, gotBefore)
+		assert.NotNil(t, gotAfter)
+		assert.True(t, gotBefore.Archived)
+		assert.True(t, gotAfter.Archived)
+	})
+
+	t.Run("should return error if context is canceled", func(t *testing.T) {
+		// given
+		r := NewFlowerbedRepository(suite.Store, mappers)
+		ctx, cancel := context.WithCancel(context.Background())
+		cancel()
+
+		// when
+		err := r.Archive(ctx, 1)
+
+		// then
+		assert.Error(t, err)
+	})
+}
