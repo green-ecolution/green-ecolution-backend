86bf33bdd0ab8db97860ee69fe6316308f23565c
Dorien Grönwald <dorien.groenwald@visuellverstehen.de>
2024-11-09 14:12:07 +0100

    feat: write tests for new GetTreeBySensorID function

--- internal/server/http/handler/v1/tree/routes.go
+++ internal/server/http/handler/v1/tree/routes.go
@@ -18,10 +18,11 @@ func RegisterRoutes(svc service.TreeService) *fiber.App {
 	app.Post("/:id/images", AddTreeImage(svc))
 	app.Delete("/:id/images/:image_id", RemoveTreeImage(svc))
 
-	app.Get("/sensor/:sensor_id", GetTreeBySensorID(svc))
 	app.Get("/:id/sensor", GetTreeSensor(svc))
 	app.Post("/:id/sensor", AddTreeSensor(svc))
 	app.Delete("/:id/sensor/:sensor_id", RemoveTreeSensor(svc))
 
+	app.Get("/sensor/:sensor_id", GetTreeBySensorID(svc))
+	
 	return app
 }
