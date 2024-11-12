b743f321785e7ab567224eae5e8342845fc7b1aa
Cedrik Hoffmann <cedrik.hoffmann@jgdperl.com>
2024-11-13 00:44:16 +0100

    feat: test utils

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -21,19 +21,6 @@ func RootDir() string {
 	return filepath.Dir(d)
 }
 
-// CompareAndUpdate compares two values and updates the new value if it is different from the old value.
-// If the new value is nil, the old value is returned. If the old value is different from the new value,
-// the new value is returned. Otherwise, the old value is returned.
-func CompareAndUpdate[T comparable](o T, n *T) T {
-	if n == nil {
-		return o
-	}
-	if o != *n {
-		return *n
-	}
-	return o
-}
-
 // Helper function to decode JSON response
 func ParseJSONResponse(body *http.Response, target any) error {
 	defer body.Body.Close()
