c29d075e4f19f5a38d3373913eacd8dce952e256
Cedrik Hoffmann <cedrik.hoffmann@jgdperl.com>
2024-11-16 13:45:54 +0100

    feat: test register user

--- internal/server/http/handler/v1/user/handler_test.go
+++ internal/server/http/handler/v1/user/handler_test.go
@@ -7,10 +7,13 @@ import (
 	"net/http/httptest"
 	"net/url"
 	"testing"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
 	domain "github.com/green-ecolution/green-ecolution-backend/internal/entities"
 	"github.com/green-ecolution/green-ecolution-backend/internal/server/http/entities"
+	"github.com/green-ecolution/green-ecolution-backend/internal/service"
 	serviceMock "github.com/green-ecolution/green-ecolution-backend/internal/service/_mock"
 	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
@@ -220,83 +223,230 @@ func TestRequestToken(t *testing.T) {
 		assert.Equal(t, expectedResponse.TokenType, response.TokenType)
 	})
 
-  t.Run("Should return 400 bad request for invalid request body.", func(t *testing.T) {
-    // given
-    app := fiber.New()
-    mockAuthService := serviceMock.NewMockAuthService(t)
-    app.Post("/v1/user/token", RequestToken(mockAuthService))
+	t.Run("Should return 400 bad request for invalid request body.", func(t *testing.T) {
+		// given
+		app := fiber.New()
+		mockAuthService := serviceMock.NewMockAuthService(t)
+		app.Post("/v1/user/token", RequestToken(mockAuthService))
 
-    // when
-    req := httptest.NewRequest(http.MethodPost, "/v1/user/token", nil)
-    resp, err := app.Test(req, -1)
-    defer resp.Body.Close()
+		// when
+		req := httptest.NewRequest(http.MethodPost, "/v1/user/token", nil)
+		resp, err := app.Test(req, -1)
+		defer resp.Body.Close()
 
-    // then
-    assert.Nil(t, err)
-    assert.Equal(t, http.StatusBadRequest, resp.StatusCode)
-    mockAuthService.AssertExpectations(t)
-  })
+		// then
+		assert.Nil(t, err)
+		assert.Equal(t, http.StatusBadRequest, resp.StatusCode)
+		mockAuthService.AssertExpectations(t)
+	})
 
-  t.Run("Should return 500 for invalid request token.", func(t *testing.T) {
-    // given
-    app := fiber.New()
-    mockAuthService := serviceMock.NewMockAuthService(t)
-    app.Post("/v1/user/token", RequestToken(mockAuthService))
+	t.Run("Should return 500 for invalid request token.", func(t *testing.T) {
+		// given
+		app := fiber.New()
+		mockAuthService := serviceMock.NewMockAuthService(t)
+		app.Post("/v1/user/token", RequestToken(mockAuthService))
 
-    redirectURL, _ := url.Parse("http://example.com/redirect")
-    domainEntity := domain.LoginCallback{
-      Code:        "valid_code",
-      RedirectURL: redirectURL,
-    }
+		redirectURL, _ := url.Parse("http://example.com/redirect")
+		domainEntity := domain.LoginCallback{
+			Code:        "valid_code",
+			RedirectURL: redirectURL,
+		}
 
-    // when
-    mockAuthService.EXPECT().ClientTokenCallback(mock.Anything, &domainEntity).Return(nil, errors.New("service error"))
-    reqBody, _ := json.Marshal(entities.LoginTokenRequest{
-      Code: "valid_code",
-    })
-    req := httptest.NewRequest(http.MethodPost, "/v1/user/token?redirect_url="+redirectURL.String(), bytes.NewReader(reqBody))
-    req.Header.Set("Content-Type", "application/json")
-    resp, err := app.Test(req, -1)
-    defer resp.Body.Close()
+		// when
+		mockAuthService.EXPECT().ClientTokenCallback(mock.Anything, &domainEntity).Return(nil, errors.New("service error"))
+		reqBody, _ := json.Marshal(entities.LoginTokenRequest{
+			Code: "valid_code",
+		})
+		req := httptest.NewRequest(http.MethodPost, "/v1/user/token?redirect_url="+redirectURL.String(), bytes.NewReader(reqBody))
+		req.Header.Set("Content-Type", "application/json")
+		resp, err := app.Test(req, -1)
+		defer resp.Body.Close()
 
-    // then
-    assert.Nil(t, err)
-    assert.Equal(t, http.StatusInternalServerError, resp.StatusCode)
-    mockAuthService.AssertExpectations(t)
-  })
+		// then
+		assert.Nil(t, err)
+		assert.Equal(t, http.StatusInternalServerError, resp.StatusCode)
+		mockAuthService.AssertExpectations(t)
+	})
 
-  t.Run("Should return 400 for invalid redirect url.", func(t *testing.T) {
-    // given
-    app := fiber.New()
-    mockAuthService := serviceMock.NewMockAuthService(t)
-    app.Post("/v1/user/token", RequestToken(mockAuthService))
+	t.Run("Should return 400 for invalid redirect url.", func(t *testing.T) {
+		// given
+		app := fiber.New()
+		mockAuthService := serviceMock.NewMockAuthService(t)
+		app.Post("/v1/user/token", RequestToken(mockAuthService))
 
-    // when
-    req := httptest.NewRequest(http.MethodPost, "/v1/user/token?redirect_url=invalid-url", nil)
-    resp, err := app.Test(req, -1)
-    defer resp.Body.Close()
+		// when
+		req := httptest.NewRequest(http.MethodPost, "/v1/user/token?redirect_url=invalid-url", nil)
+		resp, err := app.Test(req, -1)
+		defer resp.Body.Close()
 
-    // then
-    assert.Nil(t, err)
-    assert.Equal(t, http.StatusBadRequest, resp.StatusCode)
-    mockAuthService.AssertExpectations(t)
-  })
+		// then
+		assert.Nil(t, err)
+		assert.Equal(t, http.StatusBadRequest, resp.StatusCode)
+		mockAuthService.AssertExpectations(t)
+	})
 
-  t.Run("Should return 400 for invalid code.", func(t *testing.T) {
-    // given
-    app := fiber.New()
-    mockAuthService := serviceMock.NewMockAuthService(t)
-    app.Post("/v1/user/token", RequestToken(mockAuthService))
+	t.Run("Should return 400 for invalid code.", func(t *testing.T) {
+		// given
+		app := fiber.New()
+		mockAuthService := serviceMock.NewMockAuthService(t)
+		app.Post("/v1/user/token", RequestToken(mockAuthService))
 
-    // when
-    req := httptest.NewRequest(http.MethodPost, "/v1/user/token", nil)
-    resp, err := app.Test(req, -1)
-    defer resp.Body.Close()
+		// when
+		req := httptest.NewRequest(http.MethodPost, "/v1/user/token", nil)
+		resp, err := app.Test(req, -1)
+		defer resp.Body.Close()
 
-    // then
-    assert.Nil(t, err)
-    assert.Equal(t, http.StatusBadRequest, resp.StatusCode)
-    mockAuthService.AssertExpectations(t)
-  })
+		// then
+		assert.Nil(t, err)
+		assert.Equal(t, http.StatusBadRequest, resp.StatusCode)
+		mockAuthService.AssertExpectations(t)
+	})
 }
-  
+
+func TestRegister(t *testing.T) {
+	t.Run("should register user sucessfully", func(t *testing.T) {
+		// given
+		app := fiber.New()
+		mockAuthService := serviceMock.NewMockAuthService(t)
+		app.Post("/v1/user/register", Register(mockAuthService))
+
+		domainEntity := domain.RegisterUser{
+			User: domain.User{
+				Email:     "valid_email",
+				FirstName: "Toni",
+				LastName:  "Tester",
+				Username:  "toni.tester",
+			},
+			Password: "valid_password",
+			Roles:    []string{"admin"},
+		}
+
+		expectedResponse := &domain.User{
+			ID:        uuid.New(),
+			CreatedAt: time.Now(),
+			Email:     "valid_email",
+			FirstName: "Toni",
+			LastName:  "Tester",
+			Username:  "toni.tester",
+		}
+
+		// when
+		mockAuthService.EXPECT().Register(mock.Anything, &domainEntity).Return(expectedResponse, nil)
+		reqBody, _ := json.Marshal(entities.UserRegisterRequest{
+			Email:     "valid_email",
+			FirstName: "Toni",
+			LastName:  "Tester",
+			Username:  "toni.tester",
+			Password:  "valid_password",
+			Roles:     []string{"admin"},
+		})
+		req := httptest.NewRequest(http.MethodPost, "/v1/user/register", bytes.NewReader(reqBody))
+		req.Header.Set("Content-Type", "application/json")
+		resp, err := app.Test(req, -1)
+		defer resp.Body.Close()
+
+		// then
+		assert.Nil(t, err)
+		assert.Equal(t, http.StatusCreated, resp.StatusCode)
+		mockAuthService.AssertExpectations(t)
+
+		var response entities.UserResponse
+		err = json.NewDecoder(resp.Body).Decode(&response)
+		assert.Nil(t, err)
+		assert.Equal(t, expectedResponse.ID.String(), response.ID)
+		assert.Equal(t, expectedResponse.Email, response.Email)
+		assert.Equal(t, expectedResponse.FirstName, response.FirstName)
+		assert.Equal(t, expectedResponse.LastName, response.LastName)
+		assert.Equal(t, expectedResponse.Username, response.Username)
+	})
+
+	t.Run("Should return 400 bad request for invalid request body.", func(t *testing.T) {
+		// given
+		app := fiber.New()
+		mockAuthService := serviceMock.NewMockAuthService(t)
+		app.Post("/v1/user/register", Register(mockAuthService))
+
+		// when
+		req := httptest.NewRequest(http.MethodPost, "/v1/user/register", nil)
+		resp, err := app.Test(req, -1)
+		defer resp.Body.Close()
+
+		// then
+		assert.Nil(t, err)
+		assert.Equal(t, http.StatusBadRequest, resp.StatusCode)
+		mockAuthService.AssertExpectations(t)
+	})
+
+	t.Run("Should return 500 for invalid register.", func(t *testing.T) {
+		// given
+		app := fiber.New()
+		mockAuthService := serviceMock.NewMockAuthService(t)
+		app.Post("/v1/user/register", Register(mockAuthService))
+
+		domainEntity := domain.RegisterUser{
+			User: domain.User{
+				Email:     "valid_email",
+				FirstName: "Toni",
+				LastName:  "Tester",
+				Username:  "toni.tester",
+			},
+			Password: "valid_password",
+			Roles:    []string{"admin"},
+		}
+
+		// when
+		mockAuthService.EXPECT().Register(mock.Anything, &domainEntity).Return(nil, errors.New("service error"))
+		reqBody, _ := json.Marshal(entities.UserRegisterRequest{
+			Email:     "valid_email",
+			FirstName: "Toni",
+			LastName:  "Tester",
+			Username:  "toni.tester",
+			Password:  "valid_password",
+			Roles:     []string{"admin"},
+		})
+		req := httptest.NewRequest(http.MethodPost, "/v1/user/register", bytes.NewReader(reqBody))
+		req.Header.Set("Content-Type", "application/json")
+		resp, err := app.Test(req, -1)
+		defer resp.Body.Close()
+
+		// then
+		assert.Nil(t, err)
+		assert.Equal(t, http.StatusInternalServerError, resp.StatusCode)
+		mockAuthService.AssertExpectations(t)
+	})
+
+	t.Run("Should return 400 for invalid email.", func(t *testing.T) {
+		// given
+		app := fiber.New()
+		mockAuthService := serviceMock.NewMockAuthService(t)
+		app.Post("/v1/user/register", Register(mockAuthService))
+
+		domainEntity := domain.RegisterUser{
+			User: domain.User{
+				FirstName: "Toni",
+				LastName:  "Tester",
+				Username:  "toni.tester",
+			},
+			Password: "valid_password",
+			Roles:    []string{"admin"},
+		}
+
+		// when
+		mockAuthService.EXPECT().Register(mock.Anything, &domainEntity).Return(nil, service.NewError(service.BadRequest, errors.New("validation error").Error()))
+		reqBody, _ := json.Marshal(entities.UserRegisterRequest{
+			FirstName: "Toni",
+			LastName:  "Tester",
+			Username:  "toni.tester",
+			Password:  "valid_password",
+			Roles:     []string{"admin"},
+		})
+		req := httptest.NewRequest(http.MethodPost, "/v1/user/register", bytes.NewReader(reqBody))
+		req.Header.Set("Content-Type", "application/json")
+		resp, err := app.Test(req, -1)
+
+		// then
+		assert.Nil(t, err)
+		assert.Equal(t, http.StatusBadRequest, resp.StatusCode)
+		mockAuthService.AssertExpectations(t)
+	})
+}
