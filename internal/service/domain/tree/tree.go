7cfc32eed053bff1a7136ed7e88b974301c2f237
Cedrik Hoffmann <cedrik.hoffmann@jgdperl.com>
2024-11-17 15:45:07 +0100

    fix: change locator in tree temporary

--- internal/service/domain/tree/tree.go
+++ internal/service/domain/tree/tree.go
@@ -106,7 +106,11 @@ func (s *TreeService) Create(ctx context.Context, treeCreate *entities.TreeCreat
 	}
 
 	if treeCreate.TreeClusterID != nil {
-		if err = s.locator.UpdateCluster(ctx, treeCreate.TreeClusterID); err != nil {
+    tc, err := s.treeClusterRepo.GetByID(ctx, *treeCreate.TreeClusterID)
+    if err != nil {
+      return nil, handleError(err)
+    }
+		if err = s.locator.UpdateCluster(ctx, tc); err != nil {
 			return nil, handleError(err)
 		}
 	}
@@ -122,7 +126,7 @@ func (s *TreeService) Delete(ctx context.Context, id int32) error {
 		return handleError(err)
 	}
 	if treeEntity.TreeCluster != nil {
-		if err := s.locator.UpdateCluster(ctx, &treeEntity.TreeCluster.ID); err != nil {
+		if err := s.locator.UpdateCluster(ctx, treeEntity.TreeCluster); err != nil {
 			return handleError(err)
 		}
 	}
@@ -177,12 +181,12 @@ func (s *TreeService) Update(ctx context.Context, id int32, tu *entities.TreeUpd
 		return nil, handleError(err)
 	}
 	if currentTree.TreeCluster != nil {
-		if err = s.locator.UpdateCluster(ctx, &currentTree.TreeCluster.ID); err != nil {
+		if err = s.locator.UpdateCluster(ctx, currentTree.TreeCluster); err != nil {
 			return nil, handleError(err)
 		}
 	}
 	if updatedTree.TreeCluster != nil {
-		if err = s.locator.UpdateCluster(ctx, &updatedTree.TreeCluster.ID); err != nil {
+		if err = s.locator.UpdateCluster(ctx, updatedTree.TreeCluster); err != nil {
 			return nil, handleError(err)
 		}
 	}
