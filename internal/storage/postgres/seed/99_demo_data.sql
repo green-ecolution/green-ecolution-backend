0adff71ceb116b2bc5f76910cd41705aae78a0f5
Mohammad Assaf <mohammadassafb@gmail.com>
2024-11-20 04:29:04 +0100

    feat: adjust sensor seed and test data

--- internal/storage/postgres/seed/99_demo_data.sql
+++ internal/storage/postgres/seed/99_demo_data.sql
@@ -19,10 +19,22 @@ VALUES
   (8, 'Gewerbegebiet Süd', 'bad', 0.1, 13, 'Schleswiger Straße', 'Sehr viel versiegelter Boden.', 'sandig', 54.768115, 9.435285, ST_SetSRID(ST_MakePoint(54.768115, 9.435285), 4326));   
 ALTER SEQUENCE tree_clusters_id_seq RESTART WITH 9;
 
-INSERT INTO sensors (id, status) VALUES ('sensor-1', 'online');
-INSERT INTO sensors (id, status) VALUES ('sensor-2', 'offline');
-INSERT INTO sensors (id, status) VALUES ('sensor-3', 'unknown');
-INSERT INTO sensors (id, status) VALUES ('sensor-4', 'online');
+INSERT INTO sensors (id, status, latitude, longitude, geometry)
+VALUES
+    ('sensor-1', 'online', 54.82124518093376, 9.485702120628517, ST_SetSRID(ST_MakePoint(54.82124518093376, 9.485702120628517), 4326));
+
+INSERT INTO sensors (id, status, latitude, longitude, geometry)
+VALUES
+    ('sensor-2', 'offline', 54.78780993841013, 9.444052105200551, ST_SetSRID(ST_MakePoint(54.78780993841013, 9.444052105200551), 4326));
+
+INSERT INTO sensors (id, status, latitude, longitude, geometry)
+VALUES
+    ('sensor-3', 'unknown', 54.77933725347423, 9.426465409018832, ST_SetSRID(ST_MakePoint(54.77933725347423, 9.426465409018832), 4326));
+
+INSERT INTO sensors (id, status, latitude, longitude, geometry)
+VALUES
+    ('sensor-4', 'online', 54.82078826498143, 9.489684366114483, ST_SetSRID(ST_MakePoint(54.82078826498143, 9.489684366114483), 4326));
+
 
 INSERT INTO trees (tree_cluster_id, sensor_id, planting_year, species, tree_number, latitude, longitude, geometry, readonly, watering_status, description)
 VALUES 
