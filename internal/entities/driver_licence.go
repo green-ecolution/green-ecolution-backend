0d6a9e69cc91c6f80a03bbcf6d371391814e9275
Dorien Grönwald <dorien.groenwald@visuellverstehen.de>
2024-11-29 10:36:40 +0100

    chore: rename driver_licence file

--- internal/entities/driver_licence.go
+++
@@ -1,9 +0,0 @@
-package entities
-
-type DriverLicense string
-
-const (
-	DriverLicenseCar         DriverLicense = "B"
-	DriverLicenseTrailer     DriverLicense = "BE"
-	DriverLicenseTransporter DriverLicense = "C"
-)
--- internal/server/http/entities/driver_licence.go
+++
@@ -1,9 +0,0 @@
-package entities
-
-type DriverLicense string // @Name DriverLicense
-
-const (
-	DriverLicenseCar         DriverLicense = "B"
-	DriverLicenseTrailer     DriverLicense = "BE"
-	DriverLicenseTransporter DriverLicense = "C"
-)
