411882959f2640bb69c68cc444fc7fddc099b5fe
Mohammad Assaf <mohammadassafb@gmail.com>
2024-11-29 21:47:57 +0100

    feat: implement the basic logic for handling the mqtt payload

--- internal/service/domain/sensor/mqtt.go
+++ internal/service/domain/sensor/mqtt.go
@@ -3,23 +3,66 @@ package sensor
 import (
 	"context"
 
+	"github.com/go-playground/validator/v10"
 	domain "github.com/green-ecolution/green-ecolution-backend/internal/entities"
+	"github.com/green-ecolution/green-ecolution-backend/internal/service"
 	"github.com/green-ecolution/green-ecolution-backend/internal/storage"
+	storageSensor "github.com/green-ecolution/green-ecolution-backend/internal/storage/postgres/sensor"
+	"github.com/pkg/errors"
 )
 
 type MqttService struct {
 	sensorRepo  storage.SensorRepository
 	isConnected bool
+	validator   *validator.Validate
 }
 
 func NewMqttService(sensorRepository storage.SensorRepository) *MqttService {
-	return &MqttService{sensorRepo: sensorRepository}
+	return &MqttService{
+		sensorRepo: sensorRepository,
+		validator:  validator.New()}
 }
 
-func (s *MqttService) HandleMessage(_ context.Context, _ *domain.MqttPayload) (*domain.MqttPayload, error) {
-	// TODO: Implement the business logic of HandleMessage
+func (s *MqttService) HandleMessage(ctx context.Context, payload *domain.MqttPayload) (*domain.MqttPayload, error) {
+	if payload == nil {
+		return nil, handleError(errors.New("mqtt payload is nil"))
+	}
 
-	return nil, nil
+	if err := s.validator.Struct(payload); err != nil {
+		return nil, service.NewError(service.BadRequest, errors.Wrap(err, "validation error").Error())
+	}
+
+	sensor, err := s.sensorRepo.GetByID(ctx, payload.DeviceID)
+	if err != nil {
+		return nil, handleError(err)
+	}
+
+	if sensor == nil {
+		return nil, handleError(storage.ErrSensorNotFound)
+	}
+
+	data := []*domain.SensorData{
+		{
+			Data: payload,
+		},
+	}
+	_, err = s.sensorRepo.InsertSensorData(ctx, data, payload.DeviceID)
+	if err != nil {
+		return nil, handleError(err)
+	}
+
+	if sensor.Latitude != payload.Latitude || sensor.Longitude != payload.Longitude {
+		_, err = s.sensorRepo.Update(
+			ctx,
+			sensor.ID,
+			storageSensor.WithLatitude(payload.Latitude),
+			storageSensor.WithLongitude(payload.Longitude))
+		if err != nil {
+			return nil, handleError(err)
+		}
+	}
+
+	return payload, nil
 }
 
 func (s *MqttService) SetConnected(ready bool) {
