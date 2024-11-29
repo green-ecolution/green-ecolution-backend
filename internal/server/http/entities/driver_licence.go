dd1748f2c04ab32da7dc8c1cc1afe075ff3a4dc7
Dorien Grönwald <dorien.groenwald@visuellverstehen.de>
2024-11-29 10:28:59 +0100

    chore: rename licence to license due to linter

---
+++ internal/server/http/entities/driver_licence.go
@@ -0,0 +1,9 @@
+package entities
+
+type DriverLicense string // @Name DriverLicense
+
+const (
+	DriverLicenseCar         DriverLicense = "B"
+	DriverLicenseTrailer     DriverLicense = "BE"
+	DriverLicenseTransporter DriverLicense = "C"
+)
