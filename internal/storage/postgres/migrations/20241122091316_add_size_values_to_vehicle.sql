bf8d70ba8c7b97b4a2138931f8e4a8c7922daa34
Dorien Grönwald <dorien.groenwald@visuellverstehen.de>
2024-11-29 10:14:26 +0100

    chore: update migration for vehilce

--- internal/storage/postgres/migrations/20241122091316_add_size_values_to_vehicle.sql
+++ internal/storage/postgres/migrations/20241122091316_add_size_values_to_vehicle.sql
@@ -3,7 +3,7 @@
 CREATE TYPE driving_license AS ENUM ('B', 'BE', 'C');
 
 ALTER TABLE vehicles
-ADD COLUMN model TEXT NOT NULL DEFAULT 'Unknown',
+ADD COLUMN model TEXT,
 ADD COLUMN driving_license driving_license NOT NULL DEFAULT 'B',
 ADD COLUMN height FLOAT NOT NULL DEFAULT 0,
 ADD COLUMN length FLOAT NOT NULL DEFAULT 0,
