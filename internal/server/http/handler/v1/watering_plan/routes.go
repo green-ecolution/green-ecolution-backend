a6f474d3f8bb39f85278c489b5bbda49e139b88b
Dorien Grönwald <dorien.groenwald@visuellverstehen.de>
2024-12-09 09:52:14 +0100

    feat: implement basic router

---
+++ internal/server/http/handler/v1/watering_plan/routes.go
@@ -0,0 +1,14 @@
+package wateringplan
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/green-ecolution/green-ecolution-backend/internal/service"
+)
+
+func RegisterRoutes(r fiber.Router, svc service.WateringPlanService) {
+	r.Get("/", GetAllWateringPlans(svc))
+	r.Get("/:id", GetWateringPlanByID(svc))
+	r.Post("/", CreateWateringPlan(svc))
+	r.Put("/:id", UpdateWateringPlan(svc))
+	r.Delete("/:id", DeleteWateringPlan(svc))
+}
