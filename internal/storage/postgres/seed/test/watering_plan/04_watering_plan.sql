3c90d6fef8340ac082e869ef5dd5c3015404f442
Dorien Grönwald <dorien.groenwald@visuellverstehen.de>
2024-12-20 23:18:33 +0100

    fix: correct demo data

--- internal/storage/postgres/seed/test/watering_plan/04_watering_plan.sql
+++ internal/storage/postgres/seed/test/watering_plan/04_watering_plan.sql
@@ -6,7 +6,7 @@ VALUES
   (2, '2024-08-03', 'New watering plan for the east side of the city', 'active', 63.0, 0, ''),
   (3, '2024-06-12', 'Very important watering plan due to no rainfall', 'finished', 63.0, 0, ''),
   (4, '2024-06-10', 'New watering plan for the south side of the city', 'not competed', 63.0, 0, ''),
-  (5, '2024-06-04', 'Canceled due to flood', 'canceled', 63.0, 6000.0, 'The watering plan was cancelled due to various reasons.');
+  (5, '2024-06-04', 'Canceled due to flood', 'canceled', 63.0, 0.0, 'The watering plan was cancelled due to various reasons.');
 ALTER SEQUENCE watering_plans_id_seq RESTART WITH 6;
 
 INSERT INTO vehicle_watering_plans (vehicle_id, watering_plan_id) VALUES 
