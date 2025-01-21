b1b2e0c9349f2774f429039f23638addb6e2e11f
Cedrik Hoffmann <dev@choffmann.io>
2025-01-21 22:24:04 +0100

    feat: add logging to info svc

--- internal/service/domain/info/app.go
+++ internal/service/domain/info/app.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 
 	domain "github.com/green-ecolution/green-ecolution-backend/internal/entities"
+	"github.com/green-ecolution/green-ecolution-backend/internal/logger"
 	"github.com/green-ecolution/green-ecolution-backend/internal/service"
 	"github.com/green-ecolution/green-ecolution-backend/internal/storage"
 )
@@ -20,18 +21,23 @@ func NewInfoService(infoRepository storage.InfoRepository) *InfoService {
 }
 
 func (s *InfoService) GetAppInfo(ctx context.Context) (*domain.App, error) {
+	log := logger.GetLogger(ctx)
 	appInfo, err := s.infoRepository.GetAppInfo(ctx)
 	if err != nil {
 		if errors.Is(err, storage.ErrIPNotFound) {
+			log.Error("failed to receive ip from local system", "error", err)
 			return nil, service.NewError(service.InternalError, err.Error())
 		}
 		if errors.Is(err, storage.ErrIFacesNotFound) {
+			log.Error("failed to receive network interfaces from local system", "error", err)
 			return nil, service.NewError(service.InternalError, err.Error())
 		}
 		if errors.Is(err, storage.ErrIFacesAddressNotFound) {
+			log.Error("failed to receive network interfaces address from local system", "error", err)
 			return nil, service.NewError(service.InternalError, err.Error())
 		}
 		if errors.Is(err, storage.ErrHostnameNotFound) {
+			log.Error("failed to receive network hostname from local system", "error", err)
 			return nil, service.NewError(service.InternalError, err.Error())
 		}
 	}
