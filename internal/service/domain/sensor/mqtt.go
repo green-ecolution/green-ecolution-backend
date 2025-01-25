5248a2c3ba97009e4a8d967bd6d65861b92cb281
Cedrik Hoffmann <dev@choffmann.io>
2025-01-25 14:39:19 +0100

    chore: change missleading function name in mqtt service function

--- internal/service/domain/sensor/mqtt.go
+++ internal/service/domain/sensor/mqtt.go
@@ -22,9 +22,42 @@ func (s *SensorService) HandleMessage(ctx context.Context, payload *domain.MqttP
 		return nil, err
 	}
 
-	sensor, err := s.getSensor(ctx, payload)
+	sensor, err := s.sensorRepo.GetByID(ctx, payload.Device)
 	if err != nil {
-		return nil, err
+		var entityNotFoundErr storage.ErrEntityNotFound
+		if !errors.As(err, &entityNotFoundErr) {
+			log.Error("failed to get sensor by id", "error", err)
+			return nil, err
+		}
+	}
+
+	if err != nil {
+		var entityNotFoundErr storage.ErrEntityNotFound
+		if !errors.As(err, &entityNotFoundErr) {
+			log.Error("failed to get sensor by id", "error", err)
+			return nil, err
+		}
+	}
+
+	if sensor != nil {
+		updatedSensor, err := s.updateSensorCoordsAndStatus(ctx, payload, sensor)
+		if err != nil {
+			log.Error("failed to update sensor", "error", err)
+			return nil, err
+		}
+		sensor = updatedSensor
+	} else {
+		log.Info("a new sensor has joined the party! creating sensor record", "sensor_id", payload.Device, "sensor_latitude", payload.Latitude, "sensor_longitude", payload.Longitude)
+		createdSensor, err := s.sensorRepo.Create(ctx, storageSensor.WithSensorID(payload.Device),
+			storageSensor.WithLatitude(payload.Latitude),
+			storageSensor.WithLongitude(payload.Longitude),
+			storageSensor.WithStatus(domain.SensorStatusOnline),
+		)
+		if err != nil {
+			log.Error("failed to update sensor", "error", err)
+			return nil, err
+		}
+		sensor = createdSensor
 	}
 
 	data := domain.SensorData{
@@ -46,40 +79,22 @@ func (s *SensorService) HandleMessage(ctx context.Context, payload *domain.MqttP
 	return sensorData, nil
 }
 
-// TODO: missleading name, because its update or create the sensor
-func (s *SensorService) getSensor(ctx context.Context, payload *domain.MqttPayload) (*domain.Sensor, error) {
+func (s *SensorService) updateSensorCoordsAndStatus(ctx context.Context, payload *domain.MqttPayload, sensor *domain.Sensor) (*domain.Sensor, error) {
 	log := logger.GetLogger(ctx)
-	sensor, err := s.sensorRepo.GetByID(ctx, payload.Device)
-	if err != nil {
-		var entityNotFoundErr storage.ErrEntityNotFound
-		if !errors.As(err, &entityNotFoundErr) {
-			log.Error("failed to get sensor by id", "error", err)
+	if sensor.Latitude != payload.Latitude || sensor.Longitude != payload.Longitude || sensor.Status != domain.SensorStatusOnline {
+		updatedSensor, err := s.sensorRepo.Update(
+			ctx,
+			sensor.ID,
+			storageSensor.WithLatitude(payload.Latitude),
+			storageSensor.WithLongitude(payload.Longitude),
+			storageSensor.WithStatus(domain.SensorStatusOnline))
+		if err != nil {
 			return nil, err
 		}
+
+		return updatedSensor, err
 	}
 
-	if sensor != nil {
-		if sensor.Latitude != payload.Latitude || sensor.Longitude != payload.Longitude || sensor.Status != domain.SensorStatusOnline {
-			sensor, err = s.sensorRepo.Update(
-				ctx,
-				sensor.ID,
-				storageSensor.WithLatitude(payload.Latitude),
-				storageSensor.WithLongitude(payload.Longitude),
-				storageSensor.WithStatus(domain.SensorStatusOnline))
-			if err != nil {
-				return nil, err
-			}
-		}
-		return sensor, nil
-	}
-
-	log.Info("a new sensor has joined the party! creating sensor record", "sensor_id", payload.Device, "sensor_latitude", payload.Latitude, "sensor_longitude", payload.Longitude)
-	sensor, err = s.sensorRepo.Create(ctx, storageSensor.WithSensorID(payload.Device),
-		storageSensor.WithLatitude(payload.Latitude),
-		storageSensor.WithLongitude(payload.Longitude),
-		storageSensor.WithStatus(domain.SensorStatusOnline))
-	if err != nil {
-		return nil, err
-	}
+	log.Debug("sensor don't need to update coordinates and status")
 	return sensor, nil
 }
