9809babf04a1ed5a8a430401ac39bd6b24b81c1f
Mohammad Assaf <mohammadassafb@gmail.com>
2025-02-09 02:39:01 +0100

    feat: remove create and update with images functions from tree repo

--- internal/storage/postgres/tree/update.go
+++ internal/storage/postgres/tree/update.go
@@ -13,30 +13,6 @@ import (
 )
 
 func (r *TreeRepository) Update(ctx context.Context, id int32, updateFn func(*entities.Tree) (bool, error)) (*entities.Tree, error) {
-	return r.updateWithTx(ctx, id, updateFn, nil)
-}
-
-func (r *TreeRepository) UpdateWithImages(ctx context.Context, id int32, updateFn func(*entities.Tree) (bool, error)) (*entities.Tree, error) {
-	return r.updateWithTx(ctx, id, updateFn, func(ctx context.Context, tree *entities.Tree, updatedEntity *entities.Tree) error {
-		if len(tree.Images) > 0 {
-			if updatedEntity.Images == nil {
-				updatedEntity.Images = tree.Images
-			} else {
-				updatedEntity.Images = append(updatedEntity.Images, tree.Images...)
-			}
-			if err := r.updateImages(ctx, updatedEntity); err != nil {
-				return err
-			}
-		}
-		return nil
-	})
-}
-
-func (r *TreeRepository) updateWithTx(
-	ctx context.Context,
-	id int32,
-	updateFn func(*entities.Tree) (bool, error),
-	afterUpdateFn func(ctx context.Context, tree *entities.Tree, updatedEntity *entities.Tree) error) (*entities.Tree, error) {
 	log := logger.GetLogger(ctx)
 	var updatedTree *entities.Tree
 
@@ -76,12 +52,6 @@ func (r *TreeRepository) updateWithTx(
 		if err != nil {
 			log.Error("failed to get updated tree entity from db", "error", err, "tree_id", id)
 			return err
-		}
-
-		if afterUpdateFn != nil {
-			if err := afterUpdateFn(ctx, tree, updatedTree); err != nil {
-				return err
-			}
 		}
 
 		return nil
@@ -140,18 +110,4 @@ func (r *TreeRepository) updateEntity(ctx context.Context, t *entities.Tree) err
 	}
 
 	return r.store.UpdateTree(ctx, &args)
-}
-
-func (r *TreeRepository) updateImages(ctx context.Context, tree *entities.Tree) error {
-	if err := r.UnlinkAllImages(ctx, tree.ID); err != nil {
-		return err
-	}
-
-	for _, img := range tree.Images {
-		if r.linkImages(ctx, tree.ID, img.ID) != nil {
-			return errors.New("error linking image")
-		}
-	}
-
-	return nil
 }
