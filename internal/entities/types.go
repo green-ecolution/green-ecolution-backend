cd82202f5911ccd4c866c66f002ab514c794e35d
Dorien Grönwald <dorien.groenwald@visuellverstehen.de>
2025-02-09 12:29:59 +0100

    refactor: remove flowerbeds from codebase

--- internal/entities/types.go
+++ internal/entities/types.go
@@ -2,13 +2,12 @@ package entities
 
 type Entities interface {
 	Sensor |
-		Flowerbed |
-		Image |
-		Vehicle |
-		TreeCluster |
-		Tree |
-		Region |
-		WateringPlan
+	Image |
+	Vehicle |
+	TreeCluster |
+	Tree |
+	Region |
+	WateringPlan
 }
 
 type EntityFunc[T Entities] func(*T)
